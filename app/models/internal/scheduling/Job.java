b09349b78e95dfa412d198a9f9509ff7a89beb43
Spencer Pearson <spencerpearson@dropbox.com>
2019-01-16 15:47:39 -0800

    add ETag to Job

--- app/models/internal/scheduling/Job.java
+++ app/models/internal/scheduling/Job.java
@@ -19,6 +19,7 @@ import akka.actor.ActorRef;
 import com.arpnetworking.metrics.portal.scheduling.Schedule;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 
@@ -37,6 +38,13 @@ public interface Job<T> {
      * @return The UUID.
      */
     UUID getId();
+
+    /**
+     * Gets an <a href="https://en.wikipedia.org/wiki/HTTP_ETag">ETag</a> that changes each time the job changes.
+     *
+     * @return The ETag.
+     */
+    String getETag();
 
     /**
      * Returns the schedule on which the Job should be repeated.
